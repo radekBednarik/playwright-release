78323536d036ec862370a88ee1de02df54b101e0
radekBednarik <radek.bednarik@tesena.com>
2025-06-20 12:31:45 +0200

    remove ink and all deps

--- src/cli/cli.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MainComponent from "./main.component.jsx";
-import { render } from "ink";
-
-const cli = ({ mdContent }: { mdContent: string }) => {
-  render(<MainComponent mdContent={mdContent} />);
-};
-
-export default cli;
