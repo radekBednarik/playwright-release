1681ecebaec7ccf1a17cd8887fe21c273e4413f8
radekBednarik <radek.bednarik@tesena.com>
2025-06-20 13:24:31 +0200

    _

--- src/parser.ts
+++ src/parser.ts
@@ -12,31 +12,39 @@ export async function parseMarkdown(text: string): Promise<string | null> {
 
 export function parseMarkdownToJson(input: string) {
   const obj: { heading: string; info: string }[] = [];
-  const rowItem: { heading: string; info: string } = { heading: "", info: "" };
+  const rowItem: { heading: string; info: string } = {
+    heading: "",
+    info: "",
+  };
 
-  const rows = input.split("\n");
+  try {
+    const rows = input.split("\n");
 
-  let isInSection = false;
+    let isInSection = false;
 
-  for (const row of rows) {
-    if (row.startsWith("## ") && !isInSection) {
-      rowItem.heading = row.replace("## ", "").trim();
-      isInSection = true;
+    for (const row of rows) {
+      if (row.startsWith("## ") && !isInSection) {
+        rowItem.heading = row.replace("## ", "").trim();
+        isInSection = true;
 
-      continue;
+        continue;
+      }
+
+      if (!row.startsWith("## ") && isInSection) {
+        rowItem.info += row;
+
+        continue;
+      }
+
+      if (row.startsWith("## ") && isInSection) {
+        obj.push(Object.assign({}, rowItem));
+        rowItem.heading = row.replace("## ", "").trim();
+        rowItem.info = "";
+      }
     }
-
-    if (!row.startsWith("## ") && isInSection) {
-      rowItem.info += row;
-
-      continue;
-    }
-
-    if (row.startsWith("## ") && isInSection) {
-      obj.push(Object.assign({}, rowItem));
-      rowItem.heading = row.replace("## ", "").trim();
-      rowItem.info = "";
-    }
+  } catch (error) {
+    console.error("Error parsing markdown to JSON:", error);
+    return null;
   }
 
   return obj;
