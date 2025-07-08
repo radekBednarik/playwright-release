28f3ec0a63aa42240835bbb5eee31a4b32c1b620
radekBednarik <radek.bednarik@tesena.com>
2025-07-08 17:12:05 +0200

    _

--- src/client/list.ts
+++ src/client/list.ts
@@ -1,26 +1,26 @@
 import blessed from "blessed";
 
 export function createList(grid: any, data: ReleaseData[]) {
-  const list = grid.set(1, 0, 11, 1, blessed.list, {
-    label: "Versions",
-    style: {
-      selected: { bg: "blue", fg: "white", bold: true },
-      focus: { border: { fg: "blue" } },
-    },
-    selectable: true,
-    selected: 0,
-    interactive: true,
-    items: data.map((item) => item.heading),
-    vi: true,
-    scrollbar: { ch: " ", track: { bg: "grey" }, style: { bg: "blue" } },
-  });
+	const list = grid.set(1, 0, 11, 1, blessed.list, {
+		label: "Versions",
+		style: {
+			selected: { bg: "blue", fg: "white", bold: true },
+			focus: { border: { fg: "red" } },
+		},
+		selectable: true,
+		selected: 0,
+		interactive: true,
+		items: data.map((item) => item.heading),
+		vi: true,
+		scrollbar: { ch: " ", track: { bg: "grey" }, style: { bg: "blue" } },
+	});
 
-  list.key(["down", "j"], () => {
-    list.down(1);
-  });
-  list.key(["up", "k"], () => {
-    list.up(1);
-  });
+	list.key(["down", "j"], () => {
+		list.down(1);
+	});
+	list.key(["up", "k"], () => {
+		list.up(1);
+	});
 
-  return list;
+	return list;
 }
--- src/client/search.ts
+++ src/client/search.ts
@@ -1,38 +1,38 @@
 import blessed from "blessed";
 
 export function createSearch(grid: any) {
-  return grid.set(0, 0, 1, 1, blessed.textbox, {
-    label: "Search",
-    inputOnFocus: true,
-    style: {
-      border: {
-        fg: "yellow",
-      },
-      focus: {
-        border: {
-          fg: "red",
-        },
-      },
-    },
-  });
+	return grid.set(0, 0, 1, 1, blessed.textbox, {
+		label: "Search",
+		inputOnFocus: true,
+		style: {
+			border: {
+				fg: "yellow",
+			},
+			focus: {
+				border: {
+					fg: "red",
+				},
+			},
+		},
+	});
 }
 
 export function search(substr: string, data: ReleaseData[]) {
-  if (substr.length === 0) {
-    return data;
-  }
+	if (substr.length === 0) {
+		return data;
+	}
 
-  const substrLower = substr.toLowerCase();
+	const substrLower = substr.toLowerCase();
 
-  return data.reduce((acc, item) => {
-    if (item.info.toLowerCase().includes(substrLower)) {
-      acc.push({
-        heading: item.heading,
-        // I would love to use better highlighting here
-        // but blessed-contrib markdown doesn't support it
-        info: item.info.replaceAll(substrLower, `\`${substr}\``),
-      });
-    }
-    return acc;
-  }, [] as ReleaseData[]);
+	return data.reduce((acc, item) => {
+		if (item.info.toLowerCase().includes(substrLower)) {
+			acc.push({
+				heading: item.heading,
+				// I would love to use better highlighting here
+				// but blessed-contrib markdown doesn't support it
+				info: item.info.replaceAll(substrLower, `\`${substr}\``),
+			});
+		}
+		return acc;
+	}, [] as ReleaseData[]);
 }
